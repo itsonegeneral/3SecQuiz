82036a5ade81c0baa049ac506b8c3c22e8832f75
Rithik <srithik1999@gmail.com>
2018-12-27 13:56:52 +0530

    Started working in project

--- app/src/main/java/com/rstudio/a3secquiz/StartActivity.java
+++ app/src/main/java/com/rstudio/a3secquiz/StartActivity.java
@@ -2,12 +2,72 @@ package com.rstudio.a3secquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.reward.RewardItem;
+import com.google.android.gms.ads.reward.RewardedVideoAd;
+import com.google.android.gms.ads.reward.RewardedVideoAdListener;
 
 public class StartActivity extends AppCompatActivity {
+
+    private RewardedVideoAd mVideo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
+        mVideo = MobileAds.getRewardedVideoAdInstance(this);
+        loadAd();
+        mVideo.setRewardedVideoAdListener(new RewardedVideoAdListener() {
+            @Override
+            public void onRewardedVideoAdLoaded() {
+                Toast.makeText(StartActivity.this, "Loaded", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onRewardedVideoAdOpened() {
+
+            }
+
+            @Override
+            public void onRewardedVideoStarted() {
+
+            }
+
+            @Override
+            public void onRewardedVideoAdClosed() {
+
+            }
+
+            @Override
+            public void onRewarded(RewardItem rewardItem) {
+
+            }
+
+            @Override
+            public void onRewardedVideoAdLeftApplication() {
+
+            }
+
+            @Override
+            public void onRewardedVideoAdFailedToLoad(int i) {
+
+            }
+
+            @Override
+            public void onRewardedVideoCompleted() {
+
+            }
+        });
+
+
+    }
+
+    private void loadAd() {
+        mVideo.loadAd("ca-app-pub-3940256099942544/5224354917",
+                new AdRequest.Builder().build());
     }
 }
